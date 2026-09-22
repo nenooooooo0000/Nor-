17825f3eeb0dc1fb7e7a4f7b12a771ad33112e41
nenooooooo0000 <n53172856@gmail.com>
2026-09-22 15:26:14 +0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+document.querySelector(".ghost").addEventListener("click", () => {
+  alert("نسخة تجريبية فقط — لا توجد حسابات حقيقية في هذه المرحلة.");
+});
+
+document.querySelector(".primary").addEventListener("click", () => {
+  alert("تمت البداية بنجاح في نسخة العرض التجريبية.");
+});
